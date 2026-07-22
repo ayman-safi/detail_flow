2f77aaca6c34b9d80ba6b3ef241267c9876379c6
Ayman Safi <104906003+aymansafi-it@users.noreply.github.com>
2026-07-22 19:24:31 +0300

    fix build

---
+++ detailflow-web/.husky/install.mjs
@@ -0,0 +1,14 @@
+import { existsSync } from 'node:fs';
+
+if (
+  process.env.CI === 'true'
+  || process.env.NODE_ENV === 'production'
+  || !existsSync('../.git')
+) {
+  process.exit(0);
+}
+
+const husky = (await import('husky')).default;
+
+process.chdir('..');
+console.log(husky('detailflow-web/.husky'));
